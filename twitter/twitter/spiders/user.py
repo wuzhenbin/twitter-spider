30d89ca18288da9eeb02e7e175dd9bb45d6b705e
吴圳彬 <1137057407@qq.com>
2019-02-16 11:17:26 +0800

    12

--- twitter/twitter/spiders/user.py
+++ twitter/twitter/spiders/user.py
@@ -5,19 +5,20 @@ from scrapy_splash import SplashRequest
 class UserSpider(scrapy.Spider):
     name = 'user'
     allowed_domains = ['twitter.com']
-    # base_url = 'https://twitter.com/TheWoodWalker/following'
-    base_url = 'http://www.google.com'
+    base_url = 'https://twitter.com/TheWoodWalker/following'
 
     def start_requests(self):
         yield SplashRequest(
             url=self.base_url, 
             callback=self.parse, 
             endpoint='render.html',
+            args={
+                'proxy': 'http://172.16.23.228:1080',
+            }
         )
 
     def parse(self, response):
-    	print(response,"******")
-        # lis = response.css('.js-stream-item .Grid-cell')
-        # for item in lis: 
-        #     title = item.css('a.fullname::text').extract_first()
-        #     print(title,'title')
+        lis = response.css('.js-stream-item .Grid-cell')
+        for item in lis: 
+            title = item.css('a.fullname::text').extract_first()
+            print(title,'title')
